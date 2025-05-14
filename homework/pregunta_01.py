c3ec10af57c4c3cfced61c840fcf91349159f724
Tomas Carmona Salazar <tcarmona@unal.edu.co>
2025-05-14 17:55:51 -0500

    Punto 1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -7,7 +7,24 @@ utilizar pandas, numpy o scipy.
 
 
 def pregunta_01():
-    """
+    total = 0.0
+    ruta_archivo = "files\input\data.csv" 
+    separador='\t'
+    tiene_encabezado=False
+    with open(ruta_archivo, 'r', encoding='utf-8') as f:
+        if tiene_encabezado:
+            next(f)
+        for linea in f:
+            campos = linea.strip().split(separador)
+            total += int(campos[1])
+    return int(total)
+
+
+
+
+print(pregunta_01())
+
+"""
     Retorne la suma de la segunda columna.
 
     Rta/
